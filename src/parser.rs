2a49abd8c064e3ea9b899266cb5151865b342ef8
tukars <183424959+tukarsdev@users.noreply.github.com>
2025-12-31 01:53:45 +0000

    Add license notice to the top of files

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,11 @@
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2025 The rml-x Authors.
+ * SPDX-FileCopyrightText: 2025 rml-x
+ * SPDX-License-Identifier: MPL-2.0 
+ */
 
 pub struct Parser<'a> {
     src: &'a str,
